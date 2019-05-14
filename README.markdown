f09de759005660d975b6624262cc0373794c1192
Bernice Carbaugh <bernicecarbaugh@yahoo.com>
2019-05-14 13:48:20 -0700

    initial commit; back-end logic add, delete, find working; front-end input working

---
+++ README.markdown
@@ -0,0 +1,27 @@
+# Address Book
+#### Class Assignment Epicodus Intro to Programming, 2019.05.13
+#### By Bernice Carbaugh
+
+## Description
+This class project builds a simple address book using Javascript objects
+
+## Specifications
+Address Book with contacts
+
+## Setup / Installation Requirements
+[Click here](http://bernicecarbaugh.github.io/address-book/) to view the page on your browser
+
+## Known Bugs
+None
+
+## Support and Contact Details
+For any questions or feedback, contact Bernice Carbaugh at bernicecarbaugh@yahoo.com
+
+## Technologies Used
+- HTML
+- CSS/Bootstrap
+- Javascript/JQuery
+
+## Legal
+*This software is licensed under the MIT License*
+Copyright (c) 2019 **Bernice Carbaugh**
---
+++ js/scripts.js
@@ -0,0 +1,83 @@
+// front-end logic
+var addressBook = new AddressBook;
+
+$(document).ready(function(){
+  $("form#input").submit(function(event){
+    event.preventDefault();
+
+    // reset from previous runs
+    $("#div-new-first-name").removeClass("has-error");
+    $("#div-new-last-name").removeClass("has-error");
+    $("#div-new-phone-number").removeClass("has-error");
+    
+    // get and validate form input
+    var firstNameInput = $("#new-first-name").val();
+    var lastNameInput = $("#new-last-name").val();
+    var phoneNumberInput = $("#new-phone-number").val();
+
+    // show output if valid inputs; else show alert
+    if ( validInputs(firstNameInput, lastNameInput, phoneNumberInput)) {
+      var newContact = new Contact(firstNameInput, lastNameInput, phoneNumberInput);
+      addressBook.addContact (newContact);
+      console.log(addressBook.contacts);
+    } else {
+      alert ("Invalid entry. Try again.");
+    }
+  });
+
+  // validate form input
+  var validInputs = function(firstName, lastName, phoneNumber) {
+    return true;
+  }
+});
+
+
+// biz logic for AddressBook -------------
+function AddressBook() {
+  this.contacts = [];
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function (id) {
+  for(var i = 0; i < this.contacts.length; i++) {
+    if ( this.contacts[i] ) {
+      if (this.contacts[i].id === id ) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function (id) {
+  for (var i=0; i < this.contacts.length; i++) {
+    if ( this.contacts[i] ) {
+      if (this.contacts[i].id === id ) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// biz logic for Contacts -------------
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
